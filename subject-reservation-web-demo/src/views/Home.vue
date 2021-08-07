717f5b589a62b7dc4bf88c0bab29a4db4ac0dd30
Similan Klinsmith <similan.klinsmith@mail.kmutt.ac.th>
2021-08-07 14:11:40 +0700

    update title

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      Feature/List-Subjects
+      SIT Subjects Reservation Web on netlify
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
